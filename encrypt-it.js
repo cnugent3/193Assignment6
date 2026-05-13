fad7409ebe7f236eb55d706d55bfd15d2b635311
cnugent3 <cnugent773@gmail.com>
2026-05-12 17:09:11 -0700

    Update encrypt-it.js

--- encrypt-it.js
+++ encrypt-it.js
@@ -18,6 +18,11 @@
     // Note: In this function, we usually want to set up our event handlers
     // for UI elements on the page.
     console.log("Window loaded!");
+    document.getElementById("encrypt-it").addEventListener("click", handleClick);
+  }
+
+  function handleClick() {
+    console.log("Button clicked!");
   }
 
   // Add any other functions in this area (you should not implement your
