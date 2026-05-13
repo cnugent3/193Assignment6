ab1e00be236544ae102ec404c6fc6760df319c5a
cnugent3 <cnugent773@gmail.com>
2026-05-12 17:26:59 -0700

    Update encrypt-it.js

--- encrypt-it.js
+++ encrypt-it.js
@@ -18,16 +18,34 @@
     // Note: In this function, we usually want to set up our event handlers
     // for UI elements on the page.
     console.log("Window loaded!");
-    document.getElementById("encrypt-it").addEventListener("click", handleClick);
+    document.getElementById("encrypt-it").addEventListener("click", encryptClick);
     document.getElementById("reset").addEventListener("click", handleClick2);
   }
 
-  function handleClick() {
+  function encryptClick() {
+    let encryptedText = shiftCipher(document.getElementById("input-text").value);
+    document.getElementById("result").textContent = encryptedText;
     console.log("Button clicked!");
   }
   function handleClick2() {
     document.getElementById("input-text").value = "";
   }
+  function shiftCipher(text) {
+  text = text.toLowerCase();
+  let result = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] < 'a' || text[i] > 'z') {
+      result += text[i];
+    } else if (text[i] == 'z') {
+      result += 'a';
+    } else { // letter is between 'a' and 'y'
+      let letter = text.charCodeAt(i);
+      let resultLetter = String.fromCharCode(letter + 1);
+      result += resultLetter;
+    }
+  }
+  return result;
+}
 
   // Add any other functions in this area (you should not implement your
   // entire program in the init function, for similar reasons that
